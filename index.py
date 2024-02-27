83a3034dcac2fd470e4c36045b5959bb07bed5e9
Asilbek <147912523+AsilbekAbdugafforov@users.noreply.github.com>
2024-02-27 15:54:23 +0500

    Update index.py

--- index.py
+++ index.py
@@ -1,2 +1,49 @@
 # login_backend
 login uchun back end kodlari
+
+while True:
+    ism = input("ismingizni kiriting: ")
+    if str(ism):
+        f.write(f'\nIsm: {ism}')
+        break
+    else:
+        print("\nKiritilgan ma'lumo str tayipida bo'lishi kerak !!!")
+        continue   
+    
+    
+while True:
+    fam = input("Familyangizni kiritng kiriting: ")
+    if str(fam):
+        f.write(f'\nFamiliya: {fam}')
+        break
+    else:
+        print("\nFamiliyangizda faqat hariflar ishtirog etishi kerak !!!")
+        continue     
+
+
+while True:
+    tel_raqam = input("Tel raqamingizni kiriting: ")
+    if tel_raqam.startswith('+'):
+        f.write(f'\nTel: {tel_raqam}')
+        break
+    else:
+        print("\nTel raqamingiz + belgisi bilan boshlanishi kerak!")
+        continue
+  
+while True:
+    gmail = input("email po'chtangizni kiriting:  ")
+    if gmail.endswith('@gmail.com'):
+        f.write(f'\nEmail pochta: {gmail}')
+        break
+    else:
+        print("\nemail poschtangizni oxri @gmail.com bilan tugashi kerak !")
+        continue   
+
+
+while True:
+    pacword = input("parol yarating:  ")
+    if int(pacword):
+         f.write(f'\nparol: {gmail}')
+         break     
+    else:
+        print('Faqat Raqamlar qatnashishi kerak !')     
